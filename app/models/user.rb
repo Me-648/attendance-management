4098a322abd0d18384404cbaf5553d76001567a1
Tatsuki Imaoka <beta200010@gmail.com>
2025-12-07 04:27:37 +0000

    refactor: RuboCop対応・Attendanceの一意制約追加・関連削除設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
 
   # 学生の場合のみ、student_id(学籍番号)とenrollment_year(入学年度)を必須にする
   validates :student_id, presence: true, uniqueness: true, if: :student?
   validates :enrollment_year, presence: true, if: :student?
-
-  validates :email, presence: true, uniqueness: true
 
   # 0: student, 1: admin
   enum :role, { student: 0, admin: 1 }, default: :student
