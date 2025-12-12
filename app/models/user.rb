898fdcf492609b6d9b4931fad22dcdbbb74faa4b
Imaoka2000 <beta200010@gmail.com>
2025-12-12 17:21:09 +0900

    refactor: DBのnull制約追加に伴い、バリデーションも変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,8 @@ class User < ApplicationRecord
   has_many :attendances, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
-
-  # 学生の場合のみ、student_id(学籍番号)とenrollment_year(入学年度)を必須にする
-  validates :student_id, presence: true, uniqueness: true, if: :student?
-  validates :enrollment_year, presence: true, if: :student?
+  validates :student_id, presence: true, uniqueness: true
+  validates :enrollment_year, presence: true
 
   # 0: student, 1: admin
   enum :role, { student: 0, admin: 1 }, default: :student
