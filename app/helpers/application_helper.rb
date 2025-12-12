3759a6a7bc793edb878bda6d1bd7ff8759f95b3a
Imaoka2000 <beta200010@gmail.com>
2025-12-12 14:15:04 +0900

    refactor: 不要なヘルパーメソッドを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  # 出席登録が可能な時間帯かを判定する
-  # @param period [Period] 時間割オブジェクト
-  # @return [Boolean] 登録可能な場合はtrue
-  def attendance_registrable?(period)
-    # 授業開始5分前から授業開始時刻までの間を許可
-    # このロジックは app/models/attendance.rb のバリデーションと合わせておく
-    now = Time.current
-    start_time = period.start_time
-    now.between?(start_time - 5.minutes, start_time)
-  end
 end
